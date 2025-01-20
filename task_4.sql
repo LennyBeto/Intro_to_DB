9ad22f156e28b92a344cb8d161ff066302e6a3de
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-20 03:20:27 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,6 @@
 sql
 SELECT 
-    COLUMN_NAME, 
+    COLUMN_TYPE, 
     DATA_TYPE, 
     IS_NULLABLE, 
     COLUMN_DEFAULT, 
@@ -8,5 +8,5 @@ SELECT
 FROM 
     INFORMATION_SCHEMA.COLUMNS 
 WHERE 
-    TABLE_NAME = 'books' 
+    TABLE_NAME = 'Books' 
     AND TABLE_SCHEMA = 'alx_book_store';
