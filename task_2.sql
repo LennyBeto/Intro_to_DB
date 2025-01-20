830f817c2581acfab4ce14f4339b2a9a1a76e097
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-01-20 03:51:43 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -29,11 +29,11 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Order_details (
+CREATE TABLE Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id), 
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
